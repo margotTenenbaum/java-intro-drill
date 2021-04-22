1f25bd104af22f85ae1ffa38a28f9342b98549fc
Elizabeth Hayt <enhayt@gmail.com>
2021-04-22 13:01:05 -0700

    Add songNames method

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -4,6 +4,7 @@ public class Playlist {
   public String name;
   public boolean empty = true;
   public ArrayList<Song> playlist = new ArrayList<Song>();
+  public int index = 0;
 
   public Playlist(String name) {
     this.name = name;
@@ -15,6 +16,18 @@ public class Playlist {
 
   public void addSong(Song song) {
     playlist.add(song);
+    this.index++;
     this.empty = false;
+  }
+
+  public String[] songNames() {
+    String[] songNames = new String[index];
+    int i = 0;
+    for (Song song : playlist) {
+      songNames[i] = song.title;
+      i++;
+    }
+
+    return songNames;
   }
 }
