70bd653db45b7b38a93de955fe83d10d42ab0ab9
Elizabeth Hayt <enhayt@gmail.com>
2021-04-22 12:48:25 -0700

    Add addSong method and playlist ArrayList

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -2,12 +2,19 @@ import java.util.ArrayList;
 
 public class Playlist {
   public String name;
+  public boolean empty = true;
+  public ArrayList<Song> playlist = new ArrayList<Song>();
 
   public Playlist(String name) {
     this.name = name;
   }
 
   public boolean isEmpty() {
-    return true;
+    return empty;
+  }
+
+  public void addSong(Song song) {
+    playlist.add(song);
+    this.empty = false;
   }
 }
